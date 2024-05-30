2562c515b1a746f14a6043cb03117383dd73636a
phongamnart <pknart17@gmail.com>
2024-05-30 16:14:28 +0700

    commit

--- upload_img.php
+++ upload_img.php
@@ -1,17 +1,43 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['upload']) && isset($_POST['id'])) {
-    $file = $_FILES['upload'];
-    $id = $_POST['id'];
-    $uploadDirectory = 'uploads/';
+include("connect.php");
+$conDB = new db_conn();
 
-    // $idDirectory = $uploadDirectory . $id . '/';
-    // if (!is_dir($idDirectory)) {
-    //     mkdir($idDirectory, 0777, true);
-    // }
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['upload'])) {
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+    } elseif (isset($_GET['id'])) {
+        $id = $_GET['id'];
+    } else {
+        $response = [
+            "uploaded" => 0,
+            "error" => ["message" => "ID not specified."]
+        ];
+        echo json_encode($response);
+        exit;
+    }
+
+    $sql = "SELECT `doc_no` FROM `documents` WHERE md5(`id`) = '$id' LIMIT 1";
+    $result = $conDB->sqlQuery($sql);
+    $docNo = mysqli_fetch_assoc($result);
+
+    if (!$docNo) {
+        echo "Document not found";
+        exit;
+    }
+
+    $doc_no = $docNo['doc_no'];
+
+    $file = $_FILES['upload'];
+    $uploadDir = 'uploads/';
+
+    $idDir = $uploadDir . $doc_no . '/';
+    if (!is_dir($idDir)) {
+        mkdir($idDir, 0777, true);
+    }
 
     $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
-    $filename = uniqid() . $id . '.' . $ext;
-    $uploadPath = $idDirectory . $filename;
+    $filename = uniqid() . '_' .$id . '.' . $ext;
+    $uploadPath = $idDir . $filename;
 
     if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
         $url = 'http://' . $_SERVER['SERVER_NAME'] . '/method_statement/' . $uploadPath;
@@ -31,7 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['upload']) && isset($
 } else {
     $response = [
         "uploaded" => 0,
-        "error"=> ["message"=> "Invalid request"]
+        "error" => ["message" => "Invalid request"]
     ];
     echo json_encode($response);
 }
